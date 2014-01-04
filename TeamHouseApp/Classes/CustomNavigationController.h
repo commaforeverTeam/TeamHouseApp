29175632c4679df30199319a6a0bfc4852c3b1ee
李仁兵 <ziyoudelaoge@163.com>
2014-01-04 14:11:08 +0800

    提交工程

---
+++ TeamHouseApp/Classes/CustomNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  CustomNavigationController.h
+//  ITWisdomCommunity
+//
+//  Created by Renbing on 13-10-17.
+//  Copyright (c) 2013年 lirenbing@inspiry.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+ 
+@interface CustomNavigationController : UINavigationController<UINavigationControllerDelegate,UIGestureRecognizerDelegate>
+
+@end
