590a66aa41f6e8fa9f6251498e5a980fbb92be20
guangzi <627905670@qq.com>
2014-01-04 15:13:27 +0800

    修改

--- TeamHouseApp/Classes/CustomNavigationController.h
+++ TeamHouseApp/Classes/CustomNavigationController.h
@@ -9,8 +9,5 @@
 #import <UIKit/UIKit.h>
  
 @interface CustomNavigationController : UINavigationController<UINavigationControllerDelegate,UIGestureRecognizerDelegate>
-{
-    @"liuflalang";
-}
 
 @end
