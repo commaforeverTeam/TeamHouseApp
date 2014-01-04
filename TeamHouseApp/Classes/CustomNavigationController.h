19c8bf10e8aa37aab04293d273a2d7beb7bfff86
xiaoxiongl <linganggang@foxmail.com>
2014-01-04 15:47:43 +0800

    恩恩

--- TeamHouseApp/Classes/CustomNavigationController.h
+++ TeamHouseApp/Classes/CustomNavigationController.h
@@ -10,10 +10,7 @@
  
 @interface CustomNavigationController : UINavigationController<UINavigationControllerDelegate,UIGestureRecognizerDelegate>
 {
-<<<<<<< HEAD
     NSDictionary * mydict;
-=======
     NSString *liufaliang;
->>>>>>> 299872d8c398dba4c72cbf399bab8c6e6daf62e0
 }
 @end
